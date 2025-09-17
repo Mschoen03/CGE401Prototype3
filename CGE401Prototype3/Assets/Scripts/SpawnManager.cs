8fc9c78dd0a9722c543c6c2eda35b01b7a360f4a
Mschoen03 <mschoen03a@gmail.com>
2025-09-17 18:30:15 -0500

    up to 50:00 min

---
+++ CGE401Prototype3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Matthew Schoen
+ * Prototype 3
+ * Spawns the barricade obstacle
+ * 
+ */
+
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPosition = new Vector3(25, 0, 0);
+
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
